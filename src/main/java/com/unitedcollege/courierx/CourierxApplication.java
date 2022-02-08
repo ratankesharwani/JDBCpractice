050d5f4cacdc43cc4125e3e65d7fd0d66fc009e6
Rajan Kesharwani <rajankesharwani1718@gmail.com>
2022-02-09 01:01:38 +0530

    Initial comment

---
+++ src/main/java/com/unitedcollege/courierx/CourierxApplication.java
@@ -0,0 +1,13 @@
+package com.unitedcollege.courierx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitedcollege/courierx/CourierxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitedcollege.courierx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
